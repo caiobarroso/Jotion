74076b752c2bb2f70ae5d2a001e45c884c27ab80
Caio Barroso <caiobarroso511@yahoo.com.br>
2024-01-21 15:05:48 -0300

    base commit

---
+++ app/(main)/_components/document-list.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Doc, Id } from "@/convex/_generated/dataModel";
+
+interface DocumentListProps {
+  parentDocumentId?: Id<"documents">;
+  level?: number;
+  data?: Doc<"documents">[];
+}
+
+export const DocumentList = ({
+  parentDocumentId,
+  level = 0,
+}: DocumentListProps) => {
+  return <div></div>;
+};
